5553046a2ba56106cd9b42745699b6f1f39171a0
Keixys <naycarol@Naycarols-MacBook-Air.local>
2022-10-07 10:17:26 -0500

    boton de cifrado y descifrado con su evento click

--- src/index.js
+++ src/index.js
@@ -1,3 +1,26 @@
 import cipher from './cipher.js';
 
-console.log(cipher);
+
+
+//boton ingreso de texto
+let cifrarboton= document.getElementById("cifrarboton");
+cifrarboton.addEventListener("click", () => {
+    const ingresoText= document.getElementById("ingresoText").value.toUpperCase();
+    let desplazamiento=document.getElementById("desplazamiento").value;
+   cipher.encode(ingresoText, desplazamiento);
+ 
+});
+
+//boton descifro texto
+let descifrarboton0 = document.getElementById("descifrarboton0");
+descifrarboton0.addEventListener("click" , () => {
+    const ingresoText= document.getElementById("ingresoText").value.toUpperCase();
+    let desplazamiento=document.getElementById("desplazamiento").value;
+    cipher.decode(ingresoText,desplazamiento)
+});
+
+
+
+
+
+
