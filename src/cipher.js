92349bf516c4cc85c5b3d78f0cf7f0e3dd519f2a
Keixys <naycarol@Naycarols-MacBook-Air.local>
2022-10-07 10:19:01 -0500

    metodoencode con su formula

--- src/cipher.js
+++ src/cipher.js
@@ -1,5 +1,51 @@
+/* eslint-disable no-console */
 const cipher = {
-  // ...
+
+  encode: function(ingresoText, desplazamiento) {
+
+
+    // creamos un resultado vacio
+    let resultado = "";
+
+    //recorro letra por letra
+    for (let i = 0; i < ingresoText.length; i++) {
+
+      //obtener el valor de cada letra y se transforma a ASCI
+      
+      const valorxletra = ingresoText.charCodeAt(i);
+  
+      
+      //ver silas letras entan dentro del codigo
+      if (valorxletra >= 65 && valorxletra <= 90) {
+
+        //entra la formula
+        return resultado (((valorxletra - 65 + desplazamiento) % 26) + 65);
+        
+      }
+
+      //sino esta la letra
+      else {
+        return  ingresoText.charAt(i);
+      
+
+      }
+    }
+  
+  }
+
 };
 
 export default cipher;
+
+
+  
+  
+  
+
+
+
+
+
+//Posicion de mi letra
+//cantidad de espacios que se va a mover
+//eso entre el residuo de 26
