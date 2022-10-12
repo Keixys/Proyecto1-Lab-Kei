eca26f9eb75d31c29267e6af249de6f98679f5fa
Keixys <naycarol@Naycarols-MacBook-Air.local>
2022-10-11 22:48:06 -0500

    Ya esta listo pasan los test

--- src/cipher.js
+++ src/cipher.js
@@ -1,8 +1,15 @@
 /* eslint-disable no-console */
 const cipher = {
 
-  encode: function(ingresoText, desplazamiento) {
-
+  encode: function(desplazamiento,ingresoText) {
+    if(desplazamiento === undefined || ingresoText === undefined){
+     throw new TypeError ("Te falto escribiir un dato")
+    }
+    else if (desplazamiento === 0 || ingresoText === typeof 0 ) {
+      throw new TypeError ("Solo puedes escribir letras")
+    }
+    else if (desplazamiento === null ||ingresoText === null){
+     throw new TypeError ("Ingresa un valor valido")}
 
     // creamos un resultado vacio
     let resultado = "";
@@ -11,44 +18,72 @@ const cipher = {
     for (let i = 0; i < ingresoText.length; i++) {
 
       //obtener el valor de cada letra y se transforma a ASCI
-      
-      const valorxletra = ingresoText.charCodeAt(i);
+         const valorxletra = ingresoText.charCodeAt(i);
   
-      
       //ver silas letras entan dentro del codigo
       if (valorxletra >= 65 && valorxletra <= 90) {
 
-        //entra la formula
-        console.log(((valorxletra - 65 + desplazamiento) % 26 + 65) )
-       resultado += String.fromCharCode(((valorxletra - 65 + desplazamiento) % 26 + 65) );
-        
+       //entra la formula
+        let loquenosale = (valorxletra - 65 + Number(desplazamiento)) % 26 + 65;
+        console.log(loquenosale);
+        resultado += String.fromCharCode(loquenosale);
       }
 
       //sino esta la letra
       else {
-        return  ingresoText.charAt(i);
-      
-
+      resultado += ingresoText[i];
       }
     }
+
     console.log(resultado)
   return resultado
-  
-  }
 
+  
+  
+  
+  },
+
+
+  decode: function (desplazamiento,ingresoText) {
+    if(desplazamiento === undefined || ingresoText === undefined) {
+      throw new TypeError ("Te falto escribiir un dato")
+     }
+     else if (desplazamiento=== 0 || ingresoText=== typeof 0 ) {
+       throw new TypeError ("Solo puedes escribir letras")
+     }
+     else if (desplazamiento === null ||ingresoText === null){
+      throw new TypeError ("Ingresa un valor valido")}
+
+    // creamos un resultado vaci
+    let resultado = "";
+
+    //recorro letra por letra
+    for (let i = 0; i < ingresoText.length; i++) {
+
+      //obtener el valor de cada letra y se transforma a ASCI
+      const valorxletra = ingresoText.charCodeAt(i);
+
+      //ver silas letras entan dentro del codigo
+      if (valorxletra >= 65 && valorxletra <= 90) {
+
+        //entra la formula
+        let loquenosale = (valorxletra - 90 - Number(desplazamiento)) % 26 + 90;
+        console.log(loquenosale)
+        resultado += String.fromCharCode(loquenosale);
+      }
+
+
+      //sino esta la letra
+      else {
+        resultado += ingresoText[i];
+      }
+    }
+
+    console.log(resultado);
+    return resultado;
+
+  },
+  
 };
 
 export default cipher;
-
-
-  
-  
-  
-
-
-
-
-
-//Posicion de mi letra
-//cantidad de espacios que se va a mover
-//eso entre el residuo de 26
