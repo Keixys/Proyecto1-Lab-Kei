a06953999bf50281e62a3add950b01669c25558b
Keixys <naycarol@Naycarols-MacBook-Air.local>
2022-10-11 22:49:38 -0500

    Ya esta listo agregue unas imagenes y unos textarea

--- src/index.js
+++ src/index.js
@@ -5,18 +5,18 @@ import cipher from './cipher.js';
 //boton ingreso de texto
 let cifrarboton= document.getElementById("cifrarboton");
 cifrarboton.addEventListener("click", () => {
-    const ingresoText= document.getElementById("ingresoText");
-    let desplazamiento=document.getElementById("desplazamiento");
-   cipher.encode(ingresoText.value.toUpperCase(), desplazamiento.value);
+    const ingresoText= document.getElementById("ingresoText").value.toUpperCase();
+    let desplazamiento=document.getElementById("desplazamiento").value;
+    document.getElementById("textsale").value = cipher.encode(desplazamiento,ingresoText);
  
 });
 
 //boton descifro texto
 let descifrarboton0 = document.getElementById("descifrarboton0");
 descifrarboton0.addEventListener("click" , () => {
-    const ingresoText= document.getElementById("ingresoText").value.toUpperCase();
+    const ingresoText= document.getElementById("textsale").value.toUpperCase();
     let desplazamiento=document.getElementById("desplazamiento").value;
-    cipher.decode(ingresoText,desplazamiento)
+    document.getElementById("ingresoText").value = cipher.decode(desplazamiento,ingresoText);
 });
 
 
